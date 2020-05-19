eb565341622a86d723a962b6dbffaf1d68a52c5b
TimCheng <timazoey@gmail.com>
2020-05-19 17:45:53 +0800

    Updated

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Button } from '@material-ui/core';
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
 
 function App() {
